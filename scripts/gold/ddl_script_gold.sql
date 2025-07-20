a4eb1b4ef63e87f9b0f9813064bffdece32695ff
Andreea Ureche <andreea.maria.ureche@gmail.com>
2025-07-20 21:07:37 +0300

    Create ddl_script_gold.sql

---
+++ scripts/gold/ddl_script_gold.sql
@@ -0,0 +1,87 @@
+/*
+===============================================================================
+DDL Script: Create Gold Views
+===============================================================================
+--  - These views can be queried directly for analytics and reporting.
+
+IF OBJECT_ID('gold.dim_customers', 'V') IS NOT NULL
+    DROP VIEW gold.dim_customers;
+GO
+
+CREATE VIEW gold.dim_customers AS
+SELECT
+    ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key, -- Generate Surrogate key for CUSTOMER TABLE
+    ci.cst_id                          AS customer_id, -- NATURAL KEY
+    ci.cst_key                         AS customer_number, -- NATURAL KEY
+    ci.cst_firstname                   AS first_name,
+    ci.cst_lastname                    AS last_name,
+    la.cntry                           AS country,
+    ci.cst_marital_status              AS marital_status,
+    CASE 
+        --Perfom data integration for gender
+        WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the primary source for gender
+        ELSE COALESCE(ca.gen, 'n/a')  			   -- Fallback to ERP data
+    END                                AS gender,
+    ca.bdate                           AS birthdate,
+    ci.cst_create_date                 AS create_date
+FROM silver.crm_cust_info ci -- CUSTOMER TABLE 
+LEFT JOIN silver.erp_cust_az12 ca
+    ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+    ON ci.cst_key = la.cid;
+GO
+
+-- =============================================================================
+-- Create Dimension View: gold.dim_products
+-- =============================================================================
+IF OBJECT_ID('gold.dim_products', 'V') IS NOT NULL
+    DROP VIEW gold.dim_products;
+GO
+
+CREATE VIEW gold.dim_products AS
+SELECT
+    ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key, -- Generate Surrogate key for PRODUCTS TABLE
+    pn.prd_id       AS product_id,
+    pn.prd_key      AS product_number, -- NATURAL KEY
+    pn.prd_nm       AS product_name,
+    pn.cat_id       AS category_id,
+    pc.cat          AS category,
+    pc.subcat       AS subcategory,
+    pc.maintenance  AS maintenance,
+    pn.prd_cost     AS cost,
+    pn.prd_line     AS product_line,
+    pn.prd_start_dt AS start_date
+FROM silver.crm_prd_info pn -- PRODUCTS TABLE
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+    ON pn.cat_id = pc.id
+WHERE pn.prd_end_dt IS NULL; -- Filter out all historical data
+GO
+
+-- =============================================================================
+-- Create Fact Table: gold.fact_sales
+-- =============================================================================
+IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
+    DROP VIEW gold.fact_sales;
+GO
+
+--sls_prd_key, sls_cust_id - NATURAL KEYS FROM SALES TABLE
+CREATE VIEW gold.fact_sales AS
+SELECT
+    sd.sls_ord_num  AS order_number,
+--    sd.sls_prd_key,
+--    sd.sls_cust_id,
+    pr.product_key  AS product_key,  -- SURROGATE KEY FROM PRODUCT DIMENSION (DIMENSION KEY)
+    cu.customer_key AS customer_key, -- SURROGATE KEY FROM CUSTOMER DIMENSION (DIMENSION KEY)
+    sd.sls_order_dt AS order_date,
+    sd.sls_ship_dt  AS shipping_date,
+    sd.sls_due_dt   AS due_date,
+    sd.sls_sales    AS sales_amount,
+    sd.sls_quantity AS quantity,
+    sd.sls_price    AS price
+FROM silver.crm_sales_details sd --SALES TABLE
+-- JOINING DIMENSION TABLES TO EXTRACT THE SURROGATE KEYS
+LEFT JOIN gold.dim_products pr --JOIN PRODUCTS DIMENSION TABLE 
+    ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers cu -- JOIN CUSTOMERS DIMENSION TABLE
+    ON sd.sls_cust_id = cu.customer_id;
+GO
