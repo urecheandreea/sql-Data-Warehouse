d3b1ea1ade6f1f64f9b090a4be3b58d44c5afdde
Andreea Ureche <andreea.maria.ureche@gmail.com>
2025-08-08 20:58:07 +0300

    Create cumulative_analysis.sql

---
+++ analyze_data/cumulative_analysis.sql
@@ -0,0 +1,20 @@
+--- CUMULATIVE ANALYSIS ---
+
+-- Calculate the total sales per year , moving average price and the running total of sales over time 
+SELECT
+	order_date,
+	total_sales,
+	SUM(total_sales) OVER (ORDER BY order_date) AS running_total_sales,
+	AVG(avg_price) OVER (ORDER BY order_date) AS moving_average_price
+FROM
+(
+    SELECT 
+        DATETRUNC(year, order_date) AS order_date,
+        SUM(sales_amount) AS total_sales,
+        AVG(price) AS avg_price
+    FROM gold.fact_sales
+    WHERE order_date IS NOT NULL
+    GROUP BY DATETRUNC(year, order_date)
+)t
+
+
